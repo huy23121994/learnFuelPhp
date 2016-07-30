4b28e21ff7bc40c6a7baacc68dc59ff90cd0538c
huy23121994 <huy23121994@gmail.com>
2016-07-30 14:56:37 +0700

    change password db on windows

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -8,7 +8,7 @@ return array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=learnfuel',
 			'username'   => 'root',
-			'password'   => '123456',
+			'password'   => '',
 		),
 	),
 );
